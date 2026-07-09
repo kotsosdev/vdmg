128e487edb2d6e79ad84f8c3125ca96aad2b7b3e
kotsosdev <peter.e.kotsos@gmail.com>
2026-07-09 12:07:42 -0500

    memory reading

--- include/memory.hpp
+++ include/memory.hpp
@@ -6,22 +6,28 @@
 
 class Memory {
     public:
-        uint8_t read(uint16_t addr);
+        uint8_t read(uint16_t addr) const;
         void write(uint16_t addr, uint8_t val);
         void write(uint16_t addr, uint16_t val);
 
         void load_rom(const std::string& filename);
 
     private:
-        std::vector<uint8_t> rom{}; /// 0x0000 - 0x7fff (Fixed + Switchable)
-        uint8_t vram[2][0x2000]{}; /// 0x8000 - 0x9fff (Switchable)
-        uint8_t sram[0x2000]{}; /// 0xa000 - 0xbfff
-        uint8_t wram[8][0x2000]{}; /// 0xc000 - 0xdfff (Fixed + Switchable)
+        std::vector<uint8_t> rom{}; /// 0x0000 - 0x7fff (Variable banks * 0x4000)
+        uint8_t vram[0x4000]{}; /// 0x8000 - 0x9fff (2 banks * 0x2000)
+        std::vector<uint8_t> sram{}; /// 0xa000 - 0xbfff (Variable banks * 0x2000)
+        uint8_t wram[0x8000]{}; /// 0xc000 - 0xdfff (8 banks * 0x1000)
         uint8_t oam[0xa0]{}; /// 0xfe00 - 0xfe9f
         uint8_t io[0x80]{}; /// 0xff00 - 0xff7f
-        uint8_t hram[0x80]{}; /// 0xff80 - 0xffff
+        uint8_t hram[0x7f]{}; /// 0xff80 - 0xfffe
+        uint8_t ie{}; /// 0xffff
 
-        uint8_t rom_bank{};
+        uint16_t rom_bank{};
+        uint8_t sram_bank{};
         uint8_t vram_bank{};
         uint8_t wram_bank{};
+
+        bool ram_enabled{};
+
+        uint8_t mode() const;
 };
--- src/memory.cpp
+++ src/memory.cpp
@@ -15,13 +15,125 @@ using std::dec;
 using std::ifstream;
 using std::ios;
 
-uint8_t Memory::read(uint16_t addr) {
+uint8_t Memory::read(uint16_t addr) const {
+    if (addr <= 0x7fff) {
 
+        if (addr <= 0x3fff) {
+            return rom[addr];
+        }
+        return rom[(addr - 0x4000) + (rom_bank * 0x4000)];
+
+    } else if (addr <= 0x9fff) {
+        
+        if (mode() == 3) {
+            return 0xff;
+        }
+        return vram[(addr - 0x8000) + (vram_bank * 0x2000)];
+
+    } else if (addr <= 0xbfff) {
+
+        if (!ram_enabled) {
+            return 0xff;
+        }
+        return sram[(addr - 0xa000) + (sram_bank * 0x2000)];
+
+    } else if (addr <= 0xdfff) {
+
+        if (addr <= 0xcfff) {
+            return wram[addr - 0xc000];
+        }
+        return wram[(addr - 0xd000) + (wram_bank * 0x1000)];
+
+    } else if (addr <= 0xfdff) {
+
+        cerr << "Echo RAM read. (" << hex << addr << dec << ")" << endl;
+        return read(addr - 0x2000);
+
+    } else if (addr <= 0xfe9f) {
+
+        if (mode() >= 2) {
+            return 0xff;
+        }
+        return oam[addr - 0xfe00];
+
+    } else if (addr <= 0xfeff) {
+
+        cerr << "Unusable RAM read. (" << hex << addr << dec << ")" << endl;
+        return 0xff;
+
+    } else if (addr <= 0xff7f) {
+        // NOTE: Some io registers are write only
+        return io[addr - 0xff00];
+
+    } else if (addr <= 0xfffe) {
+
+        return hram[addr - 0xff80];
+
+    } else {
+
+        return ie;
+    }
 }
 
-void Memory::write(uint16_t addr, uint8_t val) {
+// void Memory::write(uint16_t addr, uint8_t val) {
+//     if (addr <= 0x7fff) {
 
-}
+//         if (addr <= 0x3fff) {
+//             return rom[addr];
+//         }
+//         return rom[(addr - 0x4000) + (rom_bank * 0x4000)];
+
+//     } else if (addr <= 0x9fff) {
+        
+//         if (mode() == 3) {
+//             return 0xff;
+//         }
+//         return vram[(addr - 0x8000) + (vram_bank * 0x2000)];
+
+//     } else if (addr <= 0xbfff) {
+
+//         if (!ram_enabled) {
+//             return 0xff;
+//         }
+//         return sram[(addr - 0xa000) + (sram_bank * 0x2000)];
+
+//     } else if (addr <= 0xdfff) {
+
+//         if (addr <= 0xcfff) {
+//             return wram[addr - 0xc000];
+//         }
+//         return wram[(addr - 0xd000) + (wram_bank * 0x1000)];
+
+//     } else if (addr <= 0xfdff) {
+
+//         cerr << "Echo RAM written to. (" << hex << addr << dec << ")" << endl;
+//         return read(addr - 0x2000);
+
+//     } else if (addr <= 0xfe9f) {
+
+//         if (mode() >= 2) {
+//             return 0xff;
+//         }
+//         return oam[addr - 0xfe00];
+
+//     } else if (addr <= 0xfeff) {
+
+//         cerr << "Unusable RAM written to. (" << hex << addr << dec << ")" << endl;
+//         return 0xff;
+
+//     } else if (addr <= 0xff7f) {
+//         // NOTE: Some io registers are write only
+//         return io[addr - 0xff00];
+
+//     } else if (addr <= 0xfffe) {
+
+//         return hram[addr - 0xff80];
+
+//     } else {
+
+//         return ie;
+//     }
+// }
 
 void Memory::write(uint16_t addr, uint16_t val) {
     
@@ -49,4 +161,16 @@ void Memory::load_rom(const std::string& filename) {
     }
 
     cout << "Loaded ROM. (" << size << " bytes)" << endl;
+}
+
+uint8_t Memory::mode() const {
+    uint8_t curr_mode = io[0x41] & 0b11;
+
+    switch (curr_mode) {
+        case 0b00: return 0; // H-blank
+        case 0b01: return 1; // V-blank
+        case 0b10: return 2; // OAM search
+        case 0b11: return 3; // Pixel transfer
+        default: return 0;
+    }
 }
