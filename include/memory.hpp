ce8f036ff440df5edb0f63d2c7afee9c3491688f
kotsosdev <peter.e.kotsos@gmail.com>
2026-07-09 19:25:10 -0500

    read and write

--- include/memory.hpp
+++ include/memory.hpp
@@ -4,6 +4,15 @@
 #include <vector>
 #include <string>
 
+struct Header {
+    std::string game_title{}; /// 0x0134 - 0x0142
+    uint8_t cgb_flag{}; /// 0x0143
+    uint8_t sgb_flag{}; /// 0x0146
+    uint8_t cart_type{}; /// 0x0147
+    uint8_t header_cs{}; /// 0x014d
+    uint16_t global_cs{}; /// 0x014e - 0x014f
+};
+
 class Memory {
     public:
         uint8_t read(uint16_t addr) const;
@@ -13,10 +22,10 @@ class Memory {
         void load_rom(const std::string& filename);
 
     private:
-        std::vector<uint8_t> rom{}; /// 0x0000 - 0x7fff (Variable banks * 0x4000)
-        uint8_t vram[0x4000]{}; /// 0x8000 - 0x9fff (2 banks * 0x2000)
-        std::vector<uint8_t> sram{}; /// 0xa000 - 0xbfff (Variable banks * 0x2000)
-        uint8_t wram[0x8000]{}; /// 0xc000 - 0xdfff (8 banks * 0x1000)
+        std::vector<uint8_t> rom{}; /// 0x0000 - 0x7fff (0x4000 * variable banks)
+        uint8_t vram[0x2000 * 2]{}; /// 0x8000 - 0x9fff (0x2000 * 2 banks)
+        std::vector<uint8_t> sram{}; /// 0xa000 - 0xbfff (0x2000 * variable banks)
+        uint8_t wram[0x1000 * 8]{}; /// 0xc000 - 0xdfff (0x1000 * 8 banks)
         uint8_t oam[0xa0]{}; /// 0xfe00 - 0xfe9f
         uint8_t io[0x80]{}; /// 0xff00 - 0xff7f
         uint8_t hram[0x7f]{}; /// 0xff80 - 0xfffe
@@ -29,5 +38,9 @@ class Memory {
 
         bool ram_enabled{};
 
-        uint8_t mode() const;
+        Header header;
+        void read_header();
+
+        void mbc_intercept(uint16_t addr, uint8_t val);
+        uint8_t ppu_mode() const;
 };
--- src/memory.cpp
+++ src/memory.cpp
@@ -25,7 +25,7 @@ uint8_t Memory::read(uint16_t addr) const {
 
     } else if (addr <= 0x9fff) {
         
-        if (mode() == 3) {
+        if (ppu_mode() == 3) {
             return 0xff;
         }
         return vram[(addr - 0x8000) + (vram_bank * 0x2000)];
@@ -51,7 +51,7 @@ uint8_t Memory::read(uint16_t addr) const {
 
     } else if (addr <= 0xfe9f) {
 
-        if (mode() >= 2) {
+        if (ppu_mode() >= 2) {
             return 0xff;
         }
         return oam[addr - 0xfe00];
@@ -75,65 +75,62 @@ uint8_t Memory::read(uint16_t addr) const {
     }
 }
 
-// void Memory::write(uint16_t addr, uint8_t val) {
-//     if (addr <= 0x7fff) {
+void Memory::write(uint16_t addr, uint8_t val) {
+    if (addr <= 0x7fff) {
 
-//         if (addr <= 0x3fff) {
-//             return rom[addr];
-//         }
-//         return rom[(addr - 0x4000) + (rom_bank * 0x4000)];
+        mbc_intercept(addr, val);
 
-//     } else if (addr <= 0x9fff) {
+    } else if (addr <= 0x9fff) {
         
-//         if (mode() == 3) {
-//             return 0xff;
-//         }
-//         return vram[(addr - 0x8000) + (vram_bank * 0x2000)];
+        if (ppu_mode() == 3) {
+            return;
+        }
+        vram[(addr - 0x8000) + (vram_bank * 0x2000)] = val;
 
-//     } else if (addr <= 0xbfff) {
+    } else if (addr <= 0xbfff) {
 
-//         if (!ram_enabled) {
-//             return 0xff;
-//         }
-//         return sram[(addr - 0xa000) + (sram_bank * 0x2000)];
+        if (!ram_enabled) {
+            return;
+        }
+        sram[(addr - 0xa000) + (sram_bank * 0x2000)] = val;
 
-//     } else if (addr <= 0xdfff) {
+    } else if (addr <= 0xdfff) {
 
-//         if (addr <= 0xcfff) {
-//             return wram[addr - 0xc000];
-//         }
-//         return wram[(addr - 0xd000) + (wram_bank * 0x1000)];
+        if (addr <= 0xcfff) {
+            wram[addr - 0xc000] = val;
+            return;
+        }
+        wram[(addr - 0xd000) + (wram_bank * 0x1000)] = val;
 
-//     } else if (addr <= 0xfdff) {
+    } else if (addr <= 0xfdff) {
 
-//         cerr << "Echo RAM written to. (" << hex << addr << dec << ")" << endl;
-//         return read(addr - 0x2000);
+        cerr << "Echo RAM written to. (" << hex << addr << dec << ")" << endl;
+        write(addr - 0x2000, val);
 
-//     } else if (addr <= 0xfe9f) {
+    } else if (addr <= 0xfe9f) {
 
-//         if (mode() >= 2) {
-//             return 0xff;
-//         }
-//         return oam[addr - 0xfe00];
+        if (ppu_mode() >= 2) {
+            return;
+        }
+        oam[addr - 0xfe00] = val;
 
-//     } else if (addr <= 0xfeff) {
+    } else if (addr <= 0xfeff) {
 
-//         cerr << "Unusable RAM written to. (" << hex << addr << dec << ")" << endl;
-//         return 0xff;
+        cerr << "Unusable RAM written to. (" << hex << addr << dec << ")" << endl;
 
-//     } else if (addr <= 0xff7f) {
-//         // NOTE: Some io registers are write only
-//         return io[addr - 0xff00];
+    } else if (addr <= 0xff7f) {
+        // NOTE: Some io registers are read only
+        io[addr - 0xff00] = val;
 
-//     } else if (addr <= 0xfffe) {
+    } else if (addr <= 0xfffe) {
 
-//         return hram[addr - 0xff80];
+        hram[addr - 0xff80] = val;
 
-//     } else {
+    } else {
 
-//         return ie;
-//     }
-// }
+        ie = val;
+    }
+}
 
 void Memory::write(uint16_t addr, uint16_t val) {
     
@@ -160,11 +157,47 @@ void Memory::load_rom(const std::string& filename) {
         return;
     }
 
+    read_header();
+
     cout << "Loaded ROM. (" << size << " bytes)" << endl;
 }
 
-uint8_t Memory::mode() const {
-    uint8_t curr_mode = io[0x41] & 0b11;
+void Memory::read_header() {
+    header.game_title = reinterpret_cast<const char*>(&rom[0x0134]);
+    header.cgb_flag = rom[0x0143];
+    header.sgb_flag = rom[0x0146];
+    header.cart_type = rom[0x0147];
+    header.header_cs = rom[0x014d];
+    header.global_cs = (rom[0x014f] << 8) | rom[0x014e];
+}
+
+void Memory::mbc_intercept(uint16_t addr, uint8_t val) {
+    if (header.cart_type == 0x00) {
+        return; // ROM
+
+    } else if (0x01 <= header.cart_type && header.cart_type <= 0x03) {
+        // MBC1
+
+    } else if (0x05 <= header.cart_type && header.cart_type <= 0x06) {
+        // MBC2
+
+    } else if (0x08 <= header.cart_type && header.cart_type <= 0x09) {
+        return; // ROM
+
+    } else if (0x0f <= header.cart_type && header.cart_type <= 0x13) {
+        // MBC3
+
+    } else if (0x19 <= header.cart_type && header.cart_type <= 0x1e) {
+        // MBC5
+        
+    } else {
+        cerr << "Unimplemented cartridge type. (" << hex << header.cart_type << dec << ")";
+        return;
+    }
+}
+
+uint8_t Memory::ppu_mode() const {
+    uint8_t curr_mode = read(0xff41) & 0b11;
 
     switch (curr_mode) {
         case 0b00: return 0; // H-blank
