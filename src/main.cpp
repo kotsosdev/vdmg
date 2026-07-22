13e0236a2f9431202f18c2207c35ace906b50002
kotsosdev <peter.e.kotsos@gmail.com>
2026-07-21 23:02:37 -0500

    python launcher

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,14 @@
 #include "../include/vdmg.hpp"
 
-#include <iostream>
-#include <print>
 #include <string>
-
-using std::cin;
-
-using std::print;
-using std::println;
 
 using std::string;
 
-int main() {
+int main(int argc, char* argv[]) {
+    string filename = "";
+    if (argc > 1 && argv[1] != nullptr) filename = argv[1];
+
     VDMG vdmg;
-
-    string filename;
-    print("ROM filename: ");
-    cin >> filename;
-    println();
-
     vdmg.load_rom(filename);
     vdmg.run();
 }
---
+++ tools/main.py
@@ -0,0 +1,27 @@
+from tkinter import Tk, filedialog
+from subprocess import run
+from pathlib import Path
+from platform import system
+
+def main() -> None:
+    EXE_PATH = Path(__file__).resolve().parent.parent / "build" / ("bin" + (".exe" if system() == "Windows" else ""))
+    ROM_DIR = Path(__file__).resolve().parent.parent / "roms"
+
+    root = Tk()
+    root.withdraw()
+
+    if not (rom_path := filedialog.askopenfilename(
+        title="Select ROM",
+        initialdir=ROM_DIR,
+        filetypes=[("ROM", "*.gb *.gbc")]
+    )): return
+
+    rom_path = Path(rom_path).resolve()
+    
+    res = run([EXE_PATH, rom_path])
+
+    if res.returncode != 0: print(f"Exited with code {res.returncode}")
+    else: print("Exited successfully")
+
+if __name__ == "__main__":
+    main()
