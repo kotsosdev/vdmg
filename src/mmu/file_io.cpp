8d9036d3975b6103a7bbbd2910867b197c41f4ec
kotsosdev <peter.e.kotsos@gmail.com>
2026-07-25 19:01:40 -0500

    cerr

--- src/mmu/file_io.cpp
+++ src/mmu/file_io.cpp
@@ -61,7 +61,7 @@ void MMU::load_rom(const std::string& rom_path) {
 void MMU::load_sav(const string& sav_path) {
     ifstream file(sav_path, ios::binary);
     if (!file) {
-        cerr << "Failed to open up save\n";
+        cerr << "Failed to open save\n";
         return;
     }
 
@@ -92,7 +92,7 @@ void MMU::load_sav(const string& sav_path) {
 void MMU::save_sav(const string& sav_path) {
     ofstream file(sav_path, ios::binary);
     if (!file) {
-        cerr << "Failed to open up save\n";
+        cerr << "Failed to open save\n";
         return;
     }
 
